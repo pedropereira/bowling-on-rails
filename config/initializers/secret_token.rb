77226520b2a988a72f296fc89a59f837e8df34c6
Pedro Pereira <pedro.mdp@gmail.com>
2012-07-17 03:59:29 +0100

    generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BowlingOnRails::Application.config.secret_token = '92d52ed784f7b5d61c06f225cd4d10cf8a5478ee6c4324dd46e689aceea31fe691587b98f6ee9b181ebb656c9cefbb521820b76dbb7314c1884eda9297f9b57c'
