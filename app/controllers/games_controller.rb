c6c89d232ecb457009d2108fef6c261584c3268b
Pedro Pereira <pedro.mdp@gmail.com>
2012-07-21 02:02:09 +0100

    generated Games controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,29 @@
+class GamesController < ApplicationController
+  def index
+    @games = Game.order("created_at DESC").all
+  end
+
+  def create
+    @game = Game.new(params[:id])
+    if @game.save
+      redirect_to @game
+    else
+      redirect_to root_url
+    end
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def update
+    @game = Game.find(params[:id])
+  end
+
+  def delete
+    @game = Game.find(params[:id])
+    @game.destroy
+
+    redirect_to root_url
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120720232953) do
+ActiveRecord::Schema.define(:version => 20120720235741) do
 
   create_table "frames", :force => true do |t|
     t.integer  "first_roll"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(:version => 20120720232953) do
     t.string   "mark",        :default => "open"
     t.datetime "created_at",                      :null => false
     t.datetime "updated_at",                      :null => false
+  end
+
+  create_table "games", :force => true do |t|
+    t.integer  "current_frame"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
