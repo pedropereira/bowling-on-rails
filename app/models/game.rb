f8ebb6211c628c6af538d3b108c32594862a2711
Pedro Pereira <pedro.mdp@gmail.com>
2012-07-22 14:18:02 +0100

    added init and total_score? methods to Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,22 @@ class Game < ActiveRecord::Base
   attr_accessible :current_frame
 
   has_many :frames
+
+  after_create :init
+
+  def init
+    10.times do
+      self.frames << Frame.create
+    end
+  end
+
+  def total_score?
+    total_score = 0
+
+    frames.each do |frame|
+      total_score = total_score + frame.score
+    end
+
+    total_score
+  end
 end
